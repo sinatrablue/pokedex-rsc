48473154ff6239f765e10f8608b29aaf2dcbf8ed
Robin Simonot <robin.simonot@id6tm.org>
2025-10-23 18:41:30 +0200

    feat: finally made the home query offset random

--- app/components/Home/PokedexPreview.tsx
+++ app/components/Home/PokedexPreview.tsx
@@ -1,15 +1,18 @@
 import { PokemonCard } from "~/components/PokemonCard";
+import { randomIntFromInterval } from "~/lib/utils";
 import type { RootQuery } from "~/types";
 
 export const PokedexPreview = async () => {
+  const randomOffset = randomIntFromInterval(1, 1300);
+
   const { results: pokemons }: RootQuery = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=8&offset=0`,
+    `https://pokeapi.co/api/v2/pokemon?limit=6&offset=${randomOffset}`,
   ).then((res) => res.json());
 
   return (
-    <div className="grid grid-cols-4 gap-6">
+    <div className="grid grid-cols-3 gap-6">
       {pokemons.map((pkm) => (
-        <PokemonCard key={pkm.name} name={pkm.name} url={pkm.url} />
+        <PokemonCard key={pkm.name} url={pkm.url} />
       ))}
     </div>
   );
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function randomIntFromInterval(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
