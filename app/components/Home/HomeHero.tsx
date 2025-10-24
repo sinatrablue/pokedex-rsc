62627f9d8ddbeff5e2c17b937f354bff39335cab
Robin Simonot <robin.simonot@id6tm.org>
2025-10-24 10:44:01 +0200

    feat: visual improvement home

--- app/components/Home/HomeHero.tsx
+++ app/components/Home/HomeHero.tsx
@@ -1,9 +1,8 @@
 export const HomeHero = () => {
   return (
-    <div className="bg-secondary/35 flex min-w-full flex-col gap-10 px-36 py-14">
+    <div className="bg-secondary/35 flex min-w-full flex-col gap-10 px-36 py-24">
       <h1 className="!text-9xl">Online Pokedex</h1>
       <h2>Build with React Server Components and React Router</h2>
-      <span>search</span>
     </div>
   );
 };
